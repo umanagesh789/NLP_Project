ed43e4ddceeec222c99418dc355025c3bbc44f7e
Deepa <deepa@gmail.com>
2026-04-01 22:35:30 +0530

    Added requirements.txt

--- app.py
+++ app.py
@@ -204,5 +204,5 @@ def index():
             translation = translate(input_text)
     return render_template('index.html', translation=translation, input_text=input_text)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
